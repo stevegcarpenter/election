457ee3d12b17f73ee30e570befc3ca12a70b9ced
Steven Carpenter <stevegcarpenter@gmail.com>
2018-04-12 10:00:54 -0700

    Accounts can now actually vote with some validation

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -8,6 +8,8 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
@@ -22,5 +24,22 @@ contract Election {
     function addCandidate(string _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        // only allow a single vote per account
+        // require() exits if given a false value
+        // if the current account has already cast a vote, it will exit
+        require(!voters[msg.sender]);
+
+        // validate candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        // msg.sender gives account number making request
+        voters[msg.sender] = true;
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount++;
     }
 }
