7ab6e374b2a8ebd4ea53c9e5c7016a695a8a91bd
Steven Carpenter <stevegcarpenter@gmail.com>
2018-04-12 10:03:03 -0700

    Clean the code up a bit

--- election/test/election.test.js
+++ election/test/election.test.js
@@ -1,24 +1,24 @@
 var Election = artifacts.require('./Election.sol');
 
-contract('Election', function () {
+contract('Election', (accounts) => {
   it('initializes with two candidates', () => {
     return Election.deployed()
-      .then((instance) => instance.candidatesCount())
-      .then((count) => assert.equal(count, 2))
-      .catch((err) => console.error(err));
+      .then(instance => instance.candidatesCount())
+      .then(count => assert.equal(count, 2))
+      .catch(err => console.error(err));
   });
 
   it('initializes the candidates with the correct values', () => {
-    let electionInstance;
+    let election;
     return Election.deployed()
-      .then((instance) => electionInstance = instance)
-      .then(() => electionInstance.candidates(1))
-      .then((candidate) => {
+      .then(instance => election = instance)
+      .then(() => election.candidates(1))
+      .then(candidate => {
         assert.equal(candidate[0], 1, 'contains the correct id');
         assert.equal(candidate[1], 'Candidate 1', 'contains the correct name');
         assert.equal(candidate[2], 0, 'contains the correct vote count');
       })
-      .then(() => electionInstance.candidates(2))
+      .then(() => election.candidates(2))
       .then((candidate) => {
         assert.equal(candidate[0], 2, 'contains the correct id');
         assert.equal(candidate[1], 'Candidate 2', 'contains the correct name');
